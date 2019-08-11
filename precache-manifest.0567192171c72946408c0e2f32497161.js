b333df5434565ca4ff4463c6721562860e5750dd
Gingano <lesha.kowalenko@gmail.com>
2019-08-11 18:31:41 +0300

    Updates

---
+++ precache-manifest.0567192171c72946408c0e2f32497161.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff805811d8bf0285606460b299961822",
+    "url": "/upqode/index.html"
+  },
+  {
+    "revision": "3c28d70c6372af720423",
+    "url": "/upqode/static/css/main.8412c985.chunk.css"
+  },
+  {
+    "revision": "17d9e41c0023df30b435",
+    "url": "/upqode/static/js/2.937242f7.chunk.js"
+  },
+  {
+    "revision": "3c28d70c6372af720423",
+    "url": "/upqode/static/js/main.e3b189c6.chunk.js"
+  },
+  {
+    "revision": "16051403964ed8ebe920",
+    "url": "/upqode/static/js/runtime~main.37ac49f0.js"
+  },
+  {
+    "revision": "6dd5ee67198d41677c3ab55a5290594d",
+    "url": "/upqode/static/media/Turkish-Airlines-Logo.6dd5ee67.png"
+  },
+  {
+    "revision": "3e60741631b14f3f3284601b70003db0",
+    "url": "/upqode/static/media/airplane.3e607416.svg"
+  },
+  {
+    "revision": "c82fe072f53821f22daeccb9ac87aa80",
+    "url": "/upqode/static/media/logo.c82fe072.svg"
+  }
+]);
